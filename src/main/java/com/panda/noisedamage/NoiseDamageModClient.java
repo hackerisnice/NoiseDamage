11654be7c78ec97f712867ff903a06e8d8ad2385
hackerisnice <131273970+hackerisnice@users.noreply.github.com>
2026-07-09 15:10:50 +0800

    Update NoiseDamageModClient.java

--- src/main/java/com/panda/noisedamage/NoiseDamageModClient.java
+++ src/main/java/com/panda/noisedamage/NoiseDamageModClient.java
@@ -1,1 +1,21 @@
+package com.panda.noisedamage;
 
+import com.panda.noisedamage.hud.NoiseHudOverlay;
+import com.panda.noisedamage.network.NoiseSyncPacket;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+
+public class NoiseDamageModClient implements ClientModInitializer {
+    public static float clientNoiseLevel = 0f;
+
+    @Override
+    public void onInitializeClient() {
+        ClientPlayNetworking.registerGlobalReceiver(NoiseSyncPacket.ID, (payload, context) -> {
+            context.client().execute(() -> {
+                clientNoiseLevel = payload.noise();
+            });
+        });
+
+        NoiseHudOverlay.register();
+    }
+}
